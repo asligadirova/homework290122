63d4fb9644096c2b85618ddbc5a5916f09f6ab3c
asligadirova <aslisg@code.edu.az>
2022-01-24 22:42:37 +0400

    task2.13 daxil olmaqla

---
+++ task2.12/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace task2._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //4 dene 6 reqemli eded verilib. Ededlerin hamisinin 10 faizini tap ve topla.
+            //Sonra hamisinin 15 faizini tap ve topla.
+            //Sonra yekunda alinanlar iki cavabi vur biri birine.
+            //Alinan neticenin evvel 10 % tap sonra ise hemin cavabin 11 % tap.
+
+            int a = 123456;
+            int b = 234567;
+            int c = 345678;
+            int d = 456789;
+            if (a>=100000 && a<1000000 && b >= 100000 && b < 1000000 && c >= 100000 && c < 1000000 && d >= 100000 && d < 1000000)
+            {
+                a = a * 10 / 100;
+                b = b * 10 / 100;
+                c = c * 10 / 100;
+                d = d * 10 / 100;
+                int cem1;
+                cem1 = a + b + c + d; //Ededlerin hamisinin 10 faizini tap ve topla.
+
+                int a1;int b1; int c1; int d1;
+                a1 = a * 15 / 100;
+                b1 = b * 15 / 100;
+                c1 = c * 15/ 100;
+                d1= d * 15 / 100;
+                int cem2;
+                cem2 = a1 + b1 + c1 + d1; //Sonra hamisinin 15 faizini tap ve topla
+                int h;
+                h = cem1 * cem2;          //Sonra yekunda alinanlar iki cavabi vur biri birine.
+
+                h = h * 10 / 100;
+                h = h * 11 / 100;
+
+                Console.WriteLine(h);
+
+            }
+            else
+            {
+                Console.WriteLine("sert odenilmir");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ task2.13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace task2._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //5 dene eded verilib.Bunlardan 3 denesi 5 reqemli. 2 denesi ise 3 reqemlidir. 5 reqemli ededlerin 5 % tap ,neticeleri vur bir birine.
+            //Sonra 3 reqemli ededlerin 3 % tap ,neticeleri topla. Sonra yekunda alinan iki cavalarin her birinin 10 % -ni tapib topla.
+            int a = 12345;
+            int b = 23456;
+            int c = 34567;
+            int d = 123;
+            int e = 234;
+            if (a>=10000 && a<100000 && b>=10000 && b<100000 && c>=10000 && c<100000 && d>=100 && d<1000 && e >= 100 && e < 1000)
+            {
+                a = a * 5 / 100;    //5 reqemli ededlerin 5 % tap
+                b = b * 5 / 100;
+                c = c * 5 / 100;
+                int hasil;
+                hasil = a * b * c;   //neticeleri vur bir birine
+
+                d = d * 3 / 100;     //Sonra 3 reqemli ededlerin 3 % tap
+                e = e * 3 / 100;
+                int cem;
+                cem = d + e;        //neticeleri topla
+
+                hasil = hasil * 10 / 100;
+                cem = cem * 10 / 100;
+                int cem1;
+                cem1 = hasil + cem;
+
+
+                Console.WriteLine(cem1);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
